22c53dd4ac1fff9f2e798dd818a16a1cf993e39e
yuchi <kirklan0423@gmail.com>
2024-08-02 12:54:59 +0800

    update

---
+++ Assets/Resources/Scripts/PlayerMovements.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMoments : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector2 moveInput;
+    public float moveSpeed;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnMove(InputValue value)//«öÁä®É±o¨ìvalue
+    {
+        moveInput = value.Get<Vector2>();
+        if (moveInput == Vector2.zero)
+        {
+            animator.SetBool("isWalking", false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+            if (moveInput.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+            if (moveInput.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(moveInput * moveSpeed);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
